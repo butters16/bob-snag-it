b4138671a5e0e3addc875a0598f56ab2399c5174
John Keith <jk@dexterbd.com>
2014-06-28 00:30:09 -0400

    Initial version with all files, including user files.

---
+++ BobSnagIt/SnagIt.Designer.vb
@@ -0,0 +1,76 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SnagIt
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SnagItButton = New System.Windows.Forms.Button
+        Me.SiteIdNumericUpDown = New System.Windows.Forms.NumericUpDown
+        Me.LabelComboBox = New System.Windows.Forms.ComboBox
+        CType(Me.SiteIdNumericUpDown, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'SnagItButton
+        '
+        Me.SnagItButton.Location = New System.Drawing.Point(12, 65)
+        Me.SnagItButton.Name = "SnagItButton"
+        Me.SnagItButton.Size = New System.Drawing.Size(293, 25)
+        Me.SnagItButton.TabIndex = 0
+        Me.SnagItButton.Text = "Snag It, Bob"
+        Me.SnagItButton.UseVisualStyleBackColor = True
+        '
+        'SiteIdNumericUpDown
+        '
+        Me.SiteIdNumericUpDown.Location = New System.Drawing.Point(12, 12)
+        Me.SiteIdNumericUpDown.Maximum = New Decimal(New Integer() {276447232, 23283, 0, 0})
+        Me.SiteIdNumericUpDown.Name = "SiteIdNumericUpDown"
+        Me.SiteIdNumericUpDown.Size = New System.Drawing.Size(293, 20)
+        Me.SiteIdNumericUpDown.TabIndex = 5
+        '
+        'LabelComboBox
+        '
+        Me.LabelComboBox.FormattingEnabled = True
+        Me.LabelComboBox.Items.AddRange(New Object() {"Start", "Search", "Clear", "ClearUsingDob", "HitOpen_List", "HitOpen_ListFirst", "HitOpen_ListMiddle", "HitOpen_ListLast", "HitOpen_Details", "HitDismiss_List", "HitDismiss_ListFirst", "HitDismiss_ListMiddle", "HitDismiss_ListLast", "HitDismiss_DetailsHitOpen_List", "HitMultiCaseMultiCharge_List", "HitMultiCaseMultiCharge_ListFirst", "HitMultiCaseMultiCharge_ListMiddle", "HitMultiCaseMultiCharge_ListLast", "HitMultiCaseMultiCharge_Details1", "HitMultiCaseMultiCharge_Details2"})
+        Me.LabelComboBox.Location = New System.Drawing.Point(12, 38)
+        Me.LabelComboBox.Name = "LabelComboBox"
+        Me.LabelComboBox.Size = New System.Drawing.Size(293, 21)
+        Me.LabelComboBox.TabIndex = 6
+        '
+        'SnagIt
+        '
+        Me.AcceptButton = Me.SnagItButton
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(316, 107)
+        Me.Controls.Add(Me.SiteIdNumericUpDown)
+        Me.Controls.Add(Me.SnagItButton)
+        Me.Controls.Add(Me.LabelComboBox)
+        Me.Name = "SnagIt"
+        Me.Text = "Bob SnagIt"
+        CType(Me.SiteIdNumericUpDown, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents SnagItButton As System.Windows.Forms.Button
+    Friend WithEvents SiteIdNumericUpDown As System.Windows.Forms.NumericUpDown
+    Friend WithEvents LabelComboBox As System.Windows.Forms.ComboBox
+
+End Class
---
+++ BobSnagIt/SnagIt.vb
@@ -0,0 +1,102 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class SnagIt
+
+    'TODO(User): Adjust for your environment.
+    Private Shared ReadOnly BaseDirectory As String = "C:\Users\Administrator\Downloads\QC"
+    Private Shared ReadOnly FirefoxClassName As String = "MozillaWindowClass" 'Found using Spy++.
+    Private Shared ReadOnly IEClassName As String = "IEFrame" 'Found using Spy++.
+    Private Shared ReadOnly DirectoryCreationWaitMs As Integer = 1000
+    Private Shared ReadOnly BringWindowToTopWaitMs As Integer = 1000
+    Private Shared ReadOnly MenuPopupWaitMs As Integer = 1000
+    Private Shared ReadOnly DialogPopupWaitMs As Integer = 1000
+
+    Private filename As String = ""
+
+    Private Sub SnagItButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SnagItButton.Click
+        Setup()
+        Save()
+    End Sub
+
+    Private Sub Setup()
+        BuildFilename()
+        EnsureDirectoryExistsForFilename()
+    End Sub
+
+    Private Sub BuildFilename()
+        Dim siteId = Me.SiteIdNumericUpDown.Value.ToString
+        Dim label = Me.LabelComboBox.Text
+        Dim directory = My.Computer.FileSystem.CombinePath(BaseDirectory, siteId)
+        filename = My.Computer.FileSystem.CombinePath(directory, siteId & "_" & label)
+    End Sub
+
+    Private Sub EnsureDirectoryExistsForFilename()
+        Dim directory = My.Computer.FileSystem.GetParentPath(filename)
+        If Not My.Computer.FileSystem.DirectoryExists(directory) Then
+            My.Computer.FileSystem.CreateDirectory(directory)
+            Threading.Thread.Sleep(DirectoryCreationWaitMs)
+        End If
+    End Sub
+
+    Private Sub Save()
+        BringWindowToTop(FirefoxClassName)
+        FirefoxAbductionSaveImage()
+
+        BringWindowToTop(Me.Handle) 'TODO(John Keith): Removing this causes IE window to not come to focus.
+
+        BringWindowToTop(IEClassName)
+        IEPageSave(fileTypeUpFromBottom:=2) 'Web Archive, single file (*.mht)
+        IEPageSave(fileTypeUpFromBottom:=3) 'Webpage, complete (*.htm;*.html)
+    End Sub
+
+    Private Sub BringWindowToTop(ByVal windowClassName As String)
+        Dim handle = FindWindow(windowClassName, vbNullString)
+        BringWindowToTop(handle)
+        Threading.Thread.Sleep(BringWindowToTopWaitMs)
+    End Sub
+
+    <DllImport("user32.dll", SetLastError:=True, CharSet:=CharSet.Auto)> _
+    Private Shared Function FindWindow( _
+         ByVal lpClassName As String, _
+         ByVal lpWindowName As String) As IntPtr
+    End Function
+
+    <DllImport("user32.dll", SetLastError:=True)> _
+    Private Shared Function BringWindowToTop(ByVal hwnd As IntPtr) As Boolean
+    End Function
+
+    Private Sub FirefoxAbductionSaveImage()
+        SendKeys.Send("%(f)") 'Request "File" menu.
+        Threading.Thread.Sleep(MenuPopupWaitMs) 'Allow "File" menu to pop up.
+        SendKeys.Send("s") 'Select "Save Page As Image...".
+        Threading.Thread.Sleep(MenuPopupWaitMs) 'Allow Abduction! buttons to pop up.
+        SendKeys.Send("%(s)") 'Press Abduction! "Save..." button.
+        Threading.Thread.Sleep(DialogPopupWaitMs) 'Allow save dialog to pop up.
+        SendKeys.Send("{ESC}") 'Abort save dialog, leaving "Save..." button with focus.
+        Threading.Thread.Sleep(DialogPopupWaitMs) 'Allow save dialog to close.
+        SendKeys.Send("{LEFT 2}") 'Focus on Abduction! "Select all" (no working shortcut, but it is 2 left of "Save..." button).
+        SendKeys.Send("{ENTER}") 'Press Abduction! "Select all" button.
+        SendKeys.Send("%(s)") 'Press Abduction! "Save..." button.
+        Threading.Thread.Sleep(DialogPopupWaitMs) 'Allow save dialog to pop up.
+        SendKeys.Send("%(n)") 'Focus on file name.
+        SendKeys.Send(filename) 'Enter file name.
+        SendKeys.Send("%(s)") 'Press "Save" button.
+        Threading.Thread.Sleep(DialogPopupWaitMs) 'Allow save dialog to close.
+    End Sub
+
+    Private Sub IEPageSave(ByVal fileTypeUpFromBottom As Integer)
+        SendKeys.Send("%(p)") 'Request "Page" menu.
+        Threading.Thread.Sleep(MenuPopupWaitMs) 'Allow "Page" menu to pop up.
+        SendKeys.Send("a") 'Select "Save As...".
+        Threading.Thread.Sleep(DialogPopupWaitMs) 'Allow save dialog to pop up.
+        SendKeys.Send("%(n)") 'Focus on "File name".
+        SendKeys.Send(filename) 'Enter file name.
+        SendKeys.Send("%(t)") 'Focus on "Save as type".
+        SendKeys.Send("{DOWN 5}") 'Select the last type.
+        SendKeys.Send("{UP " & fileTypeUpFromBottom & "}") 'Select desired type using relative position to last type.
+        SendKeys.Send("{TAB}") 'Tab off save as type.
+        SendKeys.Send("%(s)") 'Press "Save" button.
+        Threading.Thread.Sleep(DialogPopupWaitMs) 'Allow save dialog to close.
+    End Sub
+
+End Class
